076768dfbc45c858a020604e4f19fdd5a2a8773e
PalermoD <palermo.deschamps@gmail.com>
2016-04-21 14:50:24 -0400

    author name

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,8 +9,6 @@ class ArticlesController < ApplicationController
  
      def show
         @article = Article.find(params[:id])
-       
-
         respond_to do |format|
         format.html  #show.html.erb
         format.json { render json: @article }
