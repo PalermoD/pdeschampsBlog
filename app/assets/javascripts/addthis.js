f385cc107dfe4ec6b64345465f794a8c53bffdd3
PalermoD <palermo.deschamps@gmail.com>
2016-05-20 19:09:08 -0400

    files

--- app/assets/javascripts/addthis.js
+++ app/assets/javascripts/addthis.js
@@ -19,5 +19,14 @@ initAdthis = function(){
 // Trigger the function on both jquery's ready event and turbolinks page:change event
 $(document).on('ready page:change', function() {
     initAdthis();
+
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
     
 });
